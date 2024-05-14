8ee4ca60c2ff7a7b3494c88c7107564eb361f544
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2024-05-14 11:33:44 +0200

    test

--- WikiFarma/Program.cs
+++ WikiFarma/Program.cs
@@ -12,6 +12,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+List<string> names = ["Soufian El Hajouji", "Aqeedat Nabi", "Pau Insa", "Edrhian Valerio"];
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
