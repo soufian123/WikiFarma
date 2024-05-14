1f91a3e7363000c25775b483230c00f16b966a59
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2024-05-14 10:11:44 +0200

    linea dit ignore

---
+++ WikiFarma/Pages/Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WikiFarma.Pages;
+
+public class Test : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public Test(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
